ce33cfb7b9c48e9e2fd2755892d68bf11c4900c9
igor <igortarasovj@gmail.com>
2025-02-13 13:45:17 +0300

    added method for open website and changed build.gradle

--- src/test/java/TextBoxTest.java
+++ src/test/java/TextBoxTest.java
@@ -1,9 +1,17 @@
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
+
 public class TextBoxTest {
 
     @Test
      void testOutConsole(){
         System.out.println("123");
+    }
+
+    @Test
+    void openTextBox(){
+        open("https://demoqa.com/text-box");
     }
 }
